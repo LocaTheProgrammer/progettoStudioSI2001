6eb1285ac53ed1b1e2a5c570d0480208053d5fdc
LocaTheProgrammer <pietro.locatelli@stu.fondazionejobsacademy.org>
2021-10-25 16:28:39 +0200

    navbar + btn

--- demoProgettoSI2001/src/app/app.module.ts
+++ demoProgettoSI2001/src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {NgbAlertModule, NgbModule, NgbPaginationModule} from '@ng-bootstrap/ng-bootstrap';
+import { LoginComponent } from './components/login/login.component';
+import { MyButtonComponentComponent } from './components/my-button-component/my-button-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    MyButtonComponentComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    NgbPaginationModule,
+    NgbAlertModule
   ],
   providers: [],
   bootstrap: [AppComponent]
