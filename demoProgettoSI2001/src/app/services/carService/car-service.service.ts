39279bf00fb653c1bf8649930dda9462a5ad97a9
LocaTheProgrammer <pietro.locatelli@stu.fondazionejobsacademy.org>
2021-10-27 11:46:36 +0200

    buttons + first http call

---
+++ demoProgettoSI2001/src/app/services/carService/car-service.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CarServiceService {
+
+  constructor(private HttpClient: HttpClient) { }
+
+  getCars() : Observable<any>{
+    return this.HttpClient.get("http://localhost:3000/utenti")
+  }
+}
