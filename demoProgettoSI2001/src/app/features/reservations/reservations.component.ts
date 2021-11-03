846877c5cfc2b634594a561230950778f63ccca7
LocaTheProgrammer <pietro.locatelli@stu.fondazionejobsacademy.org>
2021-11-03 18:00:54 +0100

    login

---
+++ demoProgettoSI2001/src/app/features/reservations/reservations.component.ts
@@ -0,0 +1,96 @@
+import {Component, OnChanges, OnInit} from '@angular/core';
+import { Router } from "@angular/router";
+import {tableConfigAdmin, tableConfigUserPrenotazioni} from "../../components/data";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {MyTableConfig} from "../../components/table/table.component";
+import {ReservationService} from "../../services/reservation/reservation.service";
+import {UtentiService} from "../../services/utenti/utenti.service";
+
+@Component({
+  selector: 'app-reservations',
+  templateUrl: './reservations.component.html',
+  styleUrls: ['./reservations.component.scss']
+})
+export class ReservationsComponent implements OnInit, OnChanges {
+
+  loginUtenteForm!: FormGroup
+  role?=sessionStorage.getItem("ruolo")
+  toggled = true
+  passwordType = 'password'
+  tableConfig!:MyTableConfig;
+  bookedCars!:JSON[]
+  bookedCarsDates!:JSON[]
+  reservations!:JSON[]
+
+
+  constructor(private fb: FormBuilder, private  reservationService:ReservationService, private utentiService:UtentiService, private router: Router) { }
+
+  ngOnInit() {
+    this.onChangesTemp()
+  }
+
+
+
+  ngOnChanges(): void {
+    this.onChangesTemp()
+  }
+
+
+  logout(){
+    sessionStorage.removeItem("idUtente")
+    sessionStorage.removeItem("utente")
+    sessionStorage.removeItem("ruolo")
+    this.router.navigate(['/home'])
+  }
+
+
+  getUserBookedCars(){
+    let cars:any[]=[]
+    let dates:any[]=[]
+    this.reservationService.getReservations().subscribe((res:any)=>{
+      for(let i=0; i<res.length;i++){
+        if(res[i].userId==sessionStorage.getItem("idUtente")){
+
+          cars.push(res[i].car);
+          dates.push(res[i].reservationDate);
+
+        }
+      }
+
+      this.bookedCarsDates=dates
+      return this.bookedCars=cars;
+    })
+  }
+
+  event($event:any){
+    console.log($event)
+  }
+
+  onChangesTemp():void{
+    if(sessionStorage.getItem("ruolo")=='admin'){
+      this.tableConfig=tableConfigAdmin;
+      this.getReservations()
+    }else{
+      this.tableConfig=tableConfigUserPrenotazioni;
+      this.getUserBookedCars()
+    }
+    this.loginUtenteForm = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required],
+    })
+  }
+
+  getReservations(){
+    let cars:any[]=[]
+    let dates:any[]=[]
+    this.reservationService.getReservations().subscribe((res:any)=>{
+      for(let i=0; i<res.length;i++) {
+        cars.push(res[i].car);
+        dates.push(res[i].reservationDate);
+      }
+      this.bookedCarsDates=dates
+      return this.reservations=cars;
+    })
+  }
+
+}
